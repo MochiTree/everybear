c40e18f7ffd3010f366d0c3eb52f449b78c5c2e3
Mochi <mochitreets0304@gmail.com>
2025-03-10 21:38:09 +0800

    更新測試

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          記數為 {count}
+          更新測試 {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
