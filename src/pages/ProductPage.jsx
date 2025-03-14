a7744d86ade7ea10c930c99e737b19f7c8ab46a7
Mochi <mochitreets0304@gmail.com>
2025-03-14 23:48:10 +0800

    產品列表-get product與排版

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,41 @@
+import axios from 'axios';
+import { useEffect,useState } from 'react';
+import '../assets/all.scss'
+
+
+
 function ProductPage(){
-    return <h1>產品列表</h1>
+    const [products,setProducts]=useState([]);
+
+    async function getProducts(){
+        try{
+            const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_API_PATH}/products`)
+            console.log(res.data.products);
+            setProducts(res.data.products);
+        }
+        catch(err){
+            console.log('出錯囉');
+        }
+    }
+    useEffect(function(){
+        getProducts();
+    },[])
+
+    return (<><h1>產品列表</h1>
+             {/* <div style={{display:'flex'}}> */}
+             <div className="container">
+             <div className='row row-cols-4'>
+                {products.map(function(item){
+                return <div class="col py-3"><div className='card'  key={item.id}>
+                        <img src={item.imageUrl} className="product-img" alt={item.title}></img>
+                            <div className='card-body'>
+                                <h5 className="card-title">{item.title}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">{item.category}</h6>
+                                <p className='card-text'>{item.description}</p>
+                            </div>
+                        </div></div>})}
+             </div></div>         
+         </>)
 }
 
 export default ProductPage
