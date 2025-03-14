3e4fc8769cf32babf09a70e791d3a5835f84b3d6
Mochi <mochitreets0304@gmail.com>
2025-03-15 01:19:10 +0800

    更新 TopBar標籤與樣式

--- src/layouts/TopBar.jsx
+++ src/layouts/TopBar.jsx
@@ -21,15 +21,16 @@ const paths=[
  function TopBar(){
     // const href=['首頁','產品列表']
     
-    return (<><ul style={{backgroundColor:'black',width:'100%',display:'flex',justifyContent:'space-between',padding:'10px 0px'}}>
-        <div style={{display:'flex'}}>{paths.map(function(item){
-        return <><li key={item.path} className="nav-item" style={{color:'white',padding:'10px'}}>
-        <NavLink className="nav-link" aria-current="page" to={item.path}>{item.name}</NavLink>
-        </li></>
-      })}
+    return (<><nav className="navbar p-0">
+        <ul className='navbar-nav flex-row justify-content-between' style={{backgroundColor:'rgb(155, 158, 190)', width:'100%',padding:'10px 0px'}}>
+            <div style={{display:'flex'}}>{paths.map(function(item){
+            return <><li key={item.path} className="nav-item" style={{padding:'10px'}}>
+            <NavLink className="nav-link" aria-current="page" to={item.path}>{item.name}</NavLink>
+            </li></>
+            })}
       </div>
-      <div><li style={{color:'white',padding:'10px'}}><NavLink className="nav-link" aria-current="page" to='carts'><FontAwesomeIcon icon={faCartShopping} /></NavLink></li></div>
-      </ul>
+      <div><li style={{padding:'10px'}}><NavLink className="nav-link" aria-current="page" to='carts'><FontAwesomeIcon icon={faCartShopping} /></NavLink></li></div>
+      </ul></nav>
      <Outlet></Outlet>
   </>)}
   //記得之後把a換成NavLink
