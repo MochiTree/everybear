1eca2ccc88ae63a5db3423e62b8d27f25f3195d0
Mochi <mochitreets0304@gmail.com>
2025-03-15 19:56:32 +0800

    更新 產品列表篩選功能

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,8 +3,9 @@ import { useEffect,useState } from 'react';
 import '../assets/all.scss';
 import Lottie from "lottie-react";
 import teaBearLoading from '../animations/tea-bear-loading.json';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import Select from "react-select";
 
 
 function ProductPage(){
@@ -12,12 +13,16 @@ function ProductPage(){
     const [isLoading,setLoading]=useState(true);
     const [search,setSearch]=useState('');//搜尋用
     const [searchRes,setSearchRes]=useState([]);//將搜尋結果放入searchRes陣列中
+    const [options,setOptions]=useState([{value:'all',label:'全部'}]);//取得產品資料後，產生分類選項用
+    const [selectRes,setSelectRes]=useState([]);//將分類結果放入selectRes陣列
     async function getProducts(){
         try{
             const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_API_PATH}/products`)
             console.log(res.data.products);
             setProducts(res.data.products);
+
             setLoading(false);
+            
         }
         catch(err){
             console.log('出錯囉');
@@ -26,10 +31,18 @@ function ProductPage(){
     useEffect(function(){
         getProducts();
     },[])
+
+    useEffect(function(){//當產品列表更新時，分類也隨時更新
+        products.map(function(item){
+            setOptions(options=>[...options,{value:item.category,label:item.category}]);
+        })
+    },[products])
     
     //搜尋是從產品列表的title去搜尋,將搜尋結果放入新陣列(每次有搜尋就更新),來取代原先的產品列表顯示
     function searchProduct(){
         console.log(search);
+        setSelectRes([]);//將分類還原為全部(all)
+
         //外層再用map會產生重複結果(巢狀),故僅用fliter
         // const searchResult = products.map(function(item){
         //     return products.filter(function(item){ 
@@ -52,18 +65,53 @@ function ProductPage(){
         setSearchRes(searchResult);
         // console.log("結果是",searchRes)console放這裡會有生命週期問題
     }
+    // useEffect(function(){
+    //     console.log(searchRes);
+    // },[searchRes])
+
+
+    function selectProduct(opt){
+        let selectResult;
+        document.getElementById('searchBar').value='';//75~77 分類時將搜尋內部文字(input)與useState清空
+        setSearch('');
+        setSearchRes([]);
+
+            if(opt.value==='all'){//如果選擇 全部(all) 就把類別篩選的陣列清空
+                selectResult=[];
+            }
+            else {selectResult = products.filter(function(item){
+                 if(item.category===opt.value){//如果篩選得到 就會丟進去selectResult陣列
+                                return item
+                             }
+        })}
+        setSelectRes(selectResult);
+        console.log(opt)
+    }
     useEffect(function(){
-        console.log(searchRes);
-    },[searchRes])
+        console.log(selectRes);
+        console.log('陣列長度',selectRes.length);
+    },[selectRes])
+      
 
-
-    return (<><h1>產品列表</h1><div  className='d-flex justify-content-center'><input type='text' placeholder='搜尋' onChange={function(e){setSearch(e.target.value)}}></input>
-            <div className='d-flex align-items-center px-2'><button className='btn btn-sm btn-primary' onClick={searchProduct}><FontAwesomeIcon icon={faMagnifyingGlass} /></button></div></div>
+      
+    return (<><h1>產品列表</h1><div  className='d-flex justify-content-center'><input id='searchBar' type='text' placeholder='搜尋' onChange={function(e){setSearch(e.target.value)}}></input>
+            <div className='d-flex align-items-center px-2'><button className='btn btn-sm btn-primary' onClick={searchProduct}><FontAwesomeIcon icon={faMagnifyingGlass} /></button></div>
+            <Select options={options} placeholder='透過類別搜尋' onChange={function(opt){selectProduct(opt)}}/></div>
              {/* <div style={{display:'flex'}}> */}
              <div className="container">
              <div className='row row-cols-4'>
-                {/* 當有搜尋結果資料時，顯示搜尋結果 */}
-                {searchRes ? (searchRes.map(function(searchItem){
+                {/* 當有搜尋/分類結果資料時，顯示搜尋/分類結果*/}
+                {selectRes.length>0 ? (selectRes.map(function(selectItem){
+                return <div className="col py-3" key={selectItem.id}><div className='card'>
+                <img src={selectItem.imageUrl} className="product-img" alt={selectItem.title}></img>
+                    <div className='card-body'>
+                        <h5 className="card-title">{selectItem.title}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{selectItem.category}</h6>
+                        <p className='card-text'>{selectItem.description}</p>
+                    </div>
+                </div></div>    
+                }))
+                :(searchRes.length>0 ? (searchRes.map(function(searchItem){
                 return <div className="col py-3" key={searchItem.id}><div className='card'>
                         <img src={searchItem.imageUrl} className="product-img" alt={searchItem.title}></img>
                             <div className='card-body'>
@@ -80,7 +128,7 @@ function ProductPage(){
                                 <h6 className="card-subtitle mb-2 text-muted">{item.category}</h6>
                                 <p className='card-text'>{item.description}</p>
                             </div>
-                        </div></div>})) }
+                        </div></div>}))) }
 
 
              </div></div>
