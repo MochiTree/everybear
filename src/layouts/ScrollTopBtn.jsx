6e08a2e5ab3025a73871ea5edb155657d1e1cb59
Mochi <mochitreets0304@gmail.com>
2025-03-18 21:58:27 +0800

    標題 ico

--- src/layouts/ScrollTopBtn.jsx
+++ src/layouts/ScrollTopBtn.jsx
@@ -13,7 +13,7 @@ function ScrollTopBtn(){
     <button onClick={toTop} style={{position:'fixed',right:'45px',bottom:'0px',zIndex:99}} className="btn btn-info p-3 rounded-circle opacity-75">top</button>
     </div>
     
-    <div className='container-fluid p-0 d-flex' style={{backgroundColor:'rgba(142,175,157,0.7)',height:'400px'}}>
+    <div className='container-fluid p-0 d-flex' style={{backgroundColor:'rgba(142,175,157,0.7)'}}>
       <img className='object-fit-cover' src={footerBear} style={{width:'100px',height:'100px',position:'fixed',right:'20px',bottom:'0px',zIndex:1}}></img>
     </div></>)
 }
