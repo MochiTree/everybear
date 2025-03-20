55c1b47c2db18820e7d1595d1578be4f8648d426
Mochi <mochitreets0304@gmail.com>
2025-03-20 23:15:42 +0800

    文字樣式變更 結帳頁面RWD

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -59,22 +59,22 @@ function CheckOut(){
 
     return (<><h1>結帳頁面</h1>
      <div className='container mt-5 pt-5 lxgw-wenkai-mono-tc-regular'>
-        {cart?.carts?.length>0 && <div><p className='py-3'>請確認購物車資料</p><Link to={'../carts'} style={{color:'green'}}>修改訂單</Link></div>}
-        <ul>{cart.carts?.map(function(item){
-                return <li key={item.id} className='d-flex align-items-center justify-content-between my-3 border'>
+        {cart?.carts?.length>0 && <div><p className='py-3 py'>請確認購物車資料</p><Link to={'../carts'} style={{color:'green'}}>修改訂單</Link></div>}
+        <ul className='row  row-cols-1 row-cols-lg-2'>{cart.carts?.map(function(item){
+                return <li key={item.id} className='col d-flex flex-column flex-sm-row align-items-center justify-content-between p-3 my-3 border'>
                     <img src={item.product.imageUrl} className='img-thumbnail object-fit-cover' style={{height:'100px',width:'100px'}}></img>
-                        <div style={{width:'20%'}}>{item.product.title}</div>
-                        <div style={{width:'20%'}}>{`單價:${item.product.price}`}</div>
-                        <div style={{width:'20%'}}>{`數量:${item.qty}`}</div>
-                        <div style={{width:'20%'}}>{`合計:${item.final_total}`}</div>
+                        <div className='py-3 fw-bolder fs-4'>{item.product.title}</div>
+                        <div className='py-3 fs-5'>{`單價:${item.product.price}`}</div>
+                        <div className='py-3 fs-5'>{`數量:${item.qty}`}</div>
+                        <div className='py-3 fw-bolder fs-5'>{`合計:${item.final_total}`}</div>
                 </li>
         })}
-                    {cart?.carts?.length>0  && <li className='float-end' style={{width:'20%'}}>總計:{cart.total}</li>}       
+                    {cart?.carts?.length>0  && <div className='d-flex justify-content-sm-end justify-content-center'><li style={{textAlign:'right'}} className='py-3 fw-bolder fs-5'>總計:{cart.total}</li></div>}       
         </ul>
 
      <div className="my-5 row justify-content-center">
                     <form onSubmit={onSubmit} className="col-md-6">
-                    <div className="mb-3">
+                    <div className="mb-3 fw-bolder fs-5">
                         <label htmlFor="email" className="form-label">
                         Email
                         </label>
@@ -95,7 +95,7 @@ function CheckOut(){
                         {errors.email &&<p className="text-danger my-2">{errors.email.message}</p>}
                     </div>
 
-                    <div className="mb-3">
+                    <div className="mb-3 fw-bolder fs-5">
                         <label htmlFor="name" className="form-label">
                         收件人姓名
                         </label>
@@ -111,7 +111,7 @@ function CheckOut(){
                         {errors.name &&<p className="text-danger my-2">{errors.name.message}</p>}
                     </div>
 
-                    <div className="mb-3">
+                    <div className="mb-3 fw-bolder fs-5">
                         <label htmlFor="tel" className="form-label">
                         收件人電話
                         </label>
@@ -132,7 +132,7 @@ function CheckOut(){
                         {errors.tel &&<p className="text-danger my-2">{errors.tel.message}</p>}
                     </div>
 
-                    <div className="mb-3">
+                    <div className="mb-3 fw-bolder fs-5">
                         <label htmlFor="address" className="form-label">
                         收件人地址
                         </label>
@@ -149,7 +149,7 @@ function CheckOut(){
                         {errors.name &&<p className="text-danger my-2">{errors.address.message}</p>}
                     </div>
 
-                    <div className="mb-3">
+                    <div className="mb-3 fw-bolder fs-5">
                         <label htmlFor="message" className="form-label">
                         留言
                         </label>
