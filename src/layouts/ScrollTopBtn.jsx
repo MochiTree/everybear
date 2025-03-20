dd96e009aa37600011f869011e75e7de2d940db8
Mochi <mochitreets0304@gmail.com>
2025-03-20 21:33:07 +0800

    錨點:超過一定滾動高度才顯示

--- src/layouts/ScrollTopBtn.jsx
+++ src/layouts/ScrollTopBtn.jsx
@@ -1,21 +1,36 @@
 import footerBear from './images/teddy_bear_PNG136.png';
-import {useState } from 'react';
+import {useState,useEffect } from 'react';
 
 function ScrollTopBtn(){
-    // const [isTop,setIsTop]=useState(true);
+    const [isScroll,SetIsScroll]=useState(0);
+    const [unOpacity,SetUnOpacity]=useState('')
 
+
+    useEffect(function(){
+      window.addEventListener('scroll',updateY);
+      function updateY(){
+        SetIsScroll(window.scrollY);
+      }
+    },[])
+    
+    useEffect(function(){
+      // console.log(isScroll)
+      if(isScroll>30) {
+      SetUnOpacity('unOpacity')
+    }else {
+      SetUnOpacity('')
+    }
+    },[isScroll])
 
     function toTop(){
         window.scroll({top: 0, left: 0, behavior: 'smooth' })
       }
 
-    return (<><div>
+    return (<><div className={`isOpacity ${unOpacity}`}>
     <button onClick={toTop} style={{position:'fixed',right:'45px',bottom:'0px',zIndex:99}} className="btn btn-info p-3 rounded-circle opacity-75">top</button>
-    </div>
-    
     <div className='container-fluid p-0 d-flex' style={{backgroundColor:'rgba(142,175,157,0.7)'}}>
       <img className='object-fit-cover' src={footerBear} style={{width:'100px',height:'100px',position:'fixed',right:'20px',bottom:'0px',zIndex:1}}></img>
-    </div></>)
+    </div></div></>)
 }
 
 export default ScrollTopBtn
